026962dc8db30342d0f66a686375d1712155f678
Mikhail-Sibiryakov <66493852+Mikhail-Sibiryakov@users.noreply.github.com>
2022-10-12 22:34:03 +0300

    Update taskE.cpp
    Посылка: https://contest.yandex.ru/contest/39305/run-report/71911208/

--- 1contest/taskE.cpp
+++ 1contest/taskE.cpp
@@ -3,119 +3,78 @@
 #include <string>
 
 const int kINF = 1'000'000'000 + 10;
-struct Element {
+
+struct Node {
   int value;
   int min_value;
   int number;
-  Element* next;
+  Node* next;
 };
-void PrintError() { std::cout << "error\n"; }
-void PrintOk() { std::cout << "ok\n"; }
-void PushStack(Element** pointer, int k) {
-  Element* el;
-  el = new Element();
-  (*el).value = k;
-  (*el).next = *pointer;
-  if (*pointer == nullptr) {
-    (*el).min_value = k;
-    (*el).number = 1;
-  } else {
-    (*el).min_value = std::min(k, (**pointer).min_value);
-    (*el).number = (**pointer).number + 1;
-  }
-  *pointer = el;
-}
-void PopStack(Element** pointer) {
-  if (*pointer == nullptr) {
-    return;
-  }
-  Element* e = (**pointer).next;
-  delete *pointer;
-  *pointer = e;
-}
-int BackStack(Element** pointer) {
-  if (*pointer == nullptr) {
-    return -1;
-  }
-  return (**pointer).value;
-}
-int GetMinStack(Element** pointer) {
-  if (*pointer == nullptr) {
-    return -1;
-  }
-  return (**pointer).min_value;
-}
-int GetSizeStack(Element** pointer) {
-  if (*pointer == nullptr) {
-    return 0;
-  }
-  return (**pointer).number;
-}
-void ClearStack(Element** pointer) {
-  while (*pointer != nullptr) {
-    PopStack(pointer);
-  }
-}
-void PushQueue(Element** queue, int n) { PushStack(&queue[0], n); }
-void Moves(Element** queue) {
-  if (queue[0] == nullptr && queue[1] == nullptr) {
-    return;
-  }
-  if (queue[1] == nullptr) {
-    while (queue[0] != nullptr) {
-      int tmp = BackStack(&queue[0]);
-      PopStack(&queue[0]);
-      PushStack(&queue[1], tmp);
+
+class Stack {
+ public:
+  Stack() { head_ = nullptr; }
+  ~Stack() { Clear(); }
+
+  void Push(int k);
+
+  int GetSize();
+
+  bool Empty() { return GetSize() == 0; }
+
+  int GetBack();
+
+  void Pop();
+
+  int GetMin();
+
+  void Clear() {
+    while (!Empty()) {
+      Pop();
     }
   }
-}
-void PopQueue(Element** queue) {
-  if (queue[0] == nullptr && queue[1] == nullptr) {
-    PrintError();
-    return;
+
+ private:
+  Node* head_;
+};
+
+class Queue {
+ public:
+  Queue() {
+    st1_ = Stack();
+    st2_ = Stack();
   }
-  Moves(queue);
-  std::cout << BackStack(&queue[1]) << '\n';
-  PopStack(&queue[1]);
-}
-void PrintFrontQueue(Element** queue) {
-  Moves(queue);
-  int front = BackStack(&queue[1]);
-  if (front == -1) {
-    PrintError();
-  } else {
-    std::cout << front << '\n';
+
+  ~Queue() { Clear(); }
+
+  void Push(int k) { st1_.Push(k); }
+
+  int GetSize() { return st1_.GetSize() + st2_.GetSize(); }
+
+  bool Empty() { return GetSize() == 0; }
+
+  void PrintFront();
+
+  void Pop();
+
+  int GetMin();
+
+  void PrintMin();
+
+  void Clear() {
+    st1_.Clear();
+    st2_.Clear();
   }
-}
-int GetSizeQueue(Element** queue) {
-  return GetSizeStack(&queue[0]) + GetSizeStack(&queue[1]);
-}
-void ClearQueue(Element** queue) {
-  ClearStack(&queue[0]);
-  ClearStack(&queue[1]);
-}
-int GetMinQueue(Element** queue) {
-  int min_queue = kINF;
-  int min_0 = GetMinStack(&queue[0]);
-  int min_1 = GetMinStack(&queue[1]);
-  if (min_0 != -1) {
-    min_queue = std::min(min_0, min_queue);
-  }
-  if (min_1 != -1) {
-    min_queue = std::min(min_1, min_queue);
-  }
-  return min_queue;
-}
-void PrintMinQueue(Element** queue) {
-  int m = GetMinQueue(queue);
-  if (m < kINF) {
-    std::cout << m << '\n';
-  } else {
-    PrintError();
-  }
-}
+
+ private:
+  Stack st1_;
+  Stack st2_;
+  void Moves();
+  void PrintError() { std::cout << "error\n"; }
+};
+
 int main() {
-  Element* queue[2] = {nullptr, nullptr};
+  Queue queue = Queue();
   int q, m;
   std::cin >> q;
   for (int i = 0; i < q; ++i) {
@@ -123,20 +82,104 @@ int main() {
     std::cin >> s;
     if (s == "enqueue") {
       std::cin >> m;
-      PushQueue(queue, m);
-      PrintOk();
+      queue.Push(m);
+      std::cout << "ok" << '\n';
     } else if (s == "dequeue") {
-      PopQueue(queue);
+      queue.Pop();
     } else if (s == "front") {
-      PrintFrontQueue(queue);
+      queue.PrintFront();
     } else if (s == "size") {
-      std::cout << GetSizeQueue(queue) << '\n';
+      std::cout << queue.GetSize() << '\n';
     } else if (s == "clear") {
-      ClearQueue(queue);
-      PrintOk();
+      queue.Clear();
+      std::cout << "ok" << '\n';
     } else if (s == "min") {
-      PrintMinQueue(queue);
+      queue.PrintMin();
     }
   }
-  ClearQueue(queue);
+}
+void Stack::Push(int k) {
+  Node* new_node;
+  new_node = new Node();
+  new_node->value = k;
+  new_node->next = head_;
+  if (Empty()) {
+    new_node->min_value = k;
+    new_node->number = 1;
+  } else {
+    new_node->min_value = std::min(k, head_->min_value);
+    new_node->number = head_->number + 1;
+  }
+  head_ = new_node;
+}
+int Stack::GetSize() {
+  if (head_ == nullptr) {
+    return 0;
+  }
+  return head_->number;
+}
+int Stack::GetBack() {
+  if (Empty()) {
+    return -kINF;
+  }
+  return head_->value;
+}
+void Stack::Pop() {
+  if (Empty()) {
+    return;
+  }
+  Node* next = head_->next;
+  delete head_;
+  head_ = next;
+}
+int Stack::GetMin() {
+  if (Empty()) {
+    return -kINF;
+  }
+  return head_->min_value;
+}
+void Queue::PrintFront() {
+  Moves();
+  if (st2_.GetBack() == -kINF) {
+    PrintError();
+  } else {
+    std::cout << st2_.GetBack() << '\n';
+  }
+}
+void Queue::Pop() {
+  if (Empty()) {
+    PrintError();
+    return;
+  }
+  Moves();
+  PrintFront();
+  st2_.Pop();
+}
+int Queue::GetMin() {
+  int min_queue = kINF;
+  int min_1 = st1_.GetMin();
+  int min_2 = st2_.GetMin();
+  if (min_1 != -kINF) {
+    min_queue = std::min(min_1, min_queue);
+  }
+  if (min_2 != -kINF) {
+    min_queue = std::min(min_2, min_queue);
+  }
+  return min_queue;
+}
+void Queue::PrintMin() {
+  if (GetMin() < kINF) {
+    std::cout << GetMin() << '\n';
+  } else {
+    PrintError();
+  }
+}
+void Queue::Moves() {
+  if (!Empty() && st2_.Empty()) {
+    while (!st1_.Empty()) {
+      int tmp = st1_.GetBack();
+      st1_.Pop();
+      st2_.Push(tmp);
+    }
+  }
 }
