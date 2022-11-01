6d76af35e6e6ff3ceb6f8354511fdcbae2c15df4
Mikhail-Sibiryakov <66493852+Mikhail-Sibiryakov@users.noreply.github.com>
2022-11-02 00:23:12 +0300

    Update taskC.cpp
    https://contest.yandex.ru/contest/39313/run-report/73608977/

--- 2contest/taskC.cpp
+++ 2contest/taskC.cpp
@@ -5,8 +5,9 @@ using std::cin, std::cout, std::vector, std::string, std::swap;
 
 class Heap {
  public:
-  Heap() : num_(1'000'000, 0), pointer_(1'000'000, 0) {
+  Heap() {
     a_.push_back(0);
+    num_.push_back(0);
     number_ = 0;
   }
 
@@ -16,14 +17,14 @@ class Heap {
   void DecreaseKey(long long i, long long delta);
 
  private:
-  long long number_;
+  unsigned long long number_;
   vector<long long> a_;
-  vector<long long> num_;
-  vector<long long> pointer_;
+  vector<unsigned long long> num_;
+  vector<unsigned long long> pointer_;
 
-  void Exchange(long long v, long long u);
-  void SiftUp(long long v);
-  void SiftDown(long long v);
+  void Exchange(unsigned long long v, unsigned long long u);
+  void SiftUp(unsigned long long v);
+  void SiftDown(unsigned long long v);
 };
 
 int main() {
@@ -54,33 +55,33 @@ int main() {
 
 void Heap::Insert(long long x) {
   a_.push_back(x);
-  long long v = static_cast<long long>(a_.size()) - 1;
+  unsigned long long v = a_.size() - 1;
   ++number_;
-  num_[v] = number_;
+  num_.push_back(number_);
+  pointer_.resize(number_ + 1);
   pointer_[number_] = v;
   SiftUp(v);
 }
-void Heap::Exchange(long long v, long long u) {
-  long long k = num_[v];
-  long long m = num_[u];
+void Heap::Exchange(unsigned long long v, unsigned long long u) {
+  unsigned long long k = num_[v];
+  unsigned long long m = num_[u];
   swap(a_[v], a_[u]);
   swap(num_[v], num_[u]);
   swap(pointer_[k], pointer_[m]);
 }
-void Heap::SiftUp(long long v) {
+void Heap::SiftUp(unsigned long long v) {
   while (v != 1 && a_[v / 2] > a_[v]) {
     Exchange(v / 2, v);
     v /= 2;
   }
 }
-void Heap::SiftDown(long long v) {
-  while (v < static_cast<long long>(a_.size())) {
-    long long u = 2 * v;
-    if (2 * v + 1 < static_cast<long long>(a_.size()) &&
-        a_[2 * v + 1] < a_[u]) {
+void Heap::SiftDown(unsigned long long v) {
+  while (v < a_.size()) {
+    unsigned long long u = 2 * v;
+    if (2 * v + 1 < a_.size() && a_[2 * v + 1] < a_[u]) {
       u = 2 * v + 1;
     }
-    if (u < static_cast<long long>(a_.size()) && a_[u] < a_[v]) {
+    if (u < a_.size() && a_[u] < a_[v]) {
       Exchange(v, u);
       v = u;
     } else {
@@ -93,13 +94,14 @@ long long Heap::GetMin() {
   return a_[1];
 }
 void Heap::ExtractMin() {
-  Exchange(1, static_cast<long long>(a_.size()) - 1);
+  Exchange(1, a_.size() - 1);
   a_.pop_back();
+  num_.pop_back();
   SiftDown(1);
   ++number_;
 }
 void Heap::DecreaseKey(long long i, long long delta) {
-  long long v = pointer_[i];
+  unsigned long long v = pointer_[i];
   a_[v] -= delta;
   SiftUp(v);
   ++number_;
