5edd2ac25505405418020435b5946672143b1104
Mikhail-Sibiryakov <66493852+Mikhail-Sibiryakov@users.noreply.github.com>
2022-12-17 19:15:20 +0300

    Update 4A.cpp
    https://contest.yandex.ru/contest/42992/run-report/79644405/

--- 4contest/4A.cpp
+++ 4contest/4A.cpp
@@ -1,50 +1,56 @@
 #include <iostream>
+#include <list>
+#include <random>
 #include <vector>
-using std::cin, std::cout, std::vector;
+using std::cin, std::cout, std::vector, std::list;
 
-const int kM = 1'000'100;
-
-class Set {
+class HashTable {
  private:
-  vector<vector<int>> arr_;
+  static const long long kM = 1e6 + 1;
+  static const long long kP = 1e6 + 3;
+  vector<list<int>> arr_;
+  long long a_;
+  long long b_;
+
+  static long long Randint(long long a, long long b);
+  int Hash(int x);
 
  public:
-  Set() : arr_(kM, vector<int>()) {}
+  HashTable() : arr_(kM), a_(Randint(1, kP - 1)), b_(Randint(0, kP - 1)) {}
   void Push(int x);
   bool Find(int x);
   void Erase(int x);
 };
 
 int main() {
-  Set set;
+  HashTable set;
   int m;
   cin >> m;
   for (int i = 0; i < m; ++i) {
     char type;
     int n;
     cin >> type >> n;
-    if (type == '+') {
-      set.Push(n);
-    } else if (type == '-') {
-      set.Erase(n);
-    } else if (type == '?') {
-      if (set.Find(n)) {
-        cout << "YES" << '\n';
-      } else {
-        cout << "NO" << '\n';
-      }
+    switch (type) {
+      case '+':
+        set.Push(n);
+        break;
+      case '-':
+        set.Erase(n);
+        break;
+      case '?':
+        cout << (set.Find(n) ? "YES" : "NO") << '\n';
     }
   }
 }
 
-void Set::Push(int x) {
+void HashTable::Push(int x) {
   if (!Find(x)) {
-    size_t hash = std::hash<int>{}(x) % kM;
+    size_t hash = Hash(x);
     arr_[hash].push_back(x);
   }
 }
-bool Set::Find(int x) {
-  size_t hash = std::hash<int>{}(x) % kM;
+bool HashTable::Find(int x) {
+  size_t hash = Hash(x);
   for (auto i : arr_[hash]) {
     if (i == x) {
       return true;
@@ -52,13 +58,19 @@ bool Set::Find(int x) {
   }
   return false;
 }
-void Set::Erase(int x) {
-  size_t hash = std::hash<int>{}(x) % kM;
-  for (size_t i = 0; i < arr_[hash].size(); ++i) {
-    if (arr_[hash][i] == x) {
-      std::swap(arr_[hash][i], arr_[hash][arr_[hash].size() - 1]);
-      arr_[hash].pop_back();
+void HashTable::Erase(int x) {
+  size_t hash = Hash(x);
+  for (auto i = arr_[hash].begin(); i != arr_[hash].end(); ++i) {
+    if (*i == x) {
+      arr_[hash].erase(i);
       break;
     }
   }
+}
+int HashTable::Hash(int x) { return ((a_ * x + b_) % kP) % kM; }
+
+long long HashTable::Randint(long long a, long long b) {
+  std::mt19937 engine{std::random_device()()};
+  std::uniform_int_distribution<int> range(a, b);
+  return range(engine);
 }
